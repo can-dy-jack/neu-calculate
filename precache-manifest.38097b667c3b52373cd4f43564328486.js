a6eeb169f35ba8524f65c093e4b63730878168a2
can-dy-jack <jjack_chen3@163.com>
2022-09-23 15:27:01 +0800

    Updates

---
+++ precache-manifest.38097b667c3b52373cd4f43564328486.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "54dd072bd69ceb72c66c",
+    "url": "/neu-calculate/static/css/main.d48da8ef.chunk.css"
+  },
+  {
+    "revision": "54dd072bd69ceb72c66c",
+    "url": "/neu-calculate/static/js/main.6b92661c.chunk.js"
+  },
+  {
+    "revision": "5d61948d22b754ec087f",
+    "url": "/neu-calculate/static/js/runtime~main.a7078cbd.js"
+  },
+  {
+    "revision": "d51b75539c99d17cd4e2",
+    "url": "/neu-calculate/static/js/2.60ab1e13.chunk.js"
+  },
+  {
+    "revision": "d107840aca5a4f33812e6c8e39af0eab",
+    "url": "/neu-calculate/index.html"
+  }
+];
